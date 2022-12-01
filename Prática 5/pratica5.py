ee3d9cfac1bf78edf5fb0f8fe691b188d5450e85
gianvstheworld <gianlucacapezzuto@usp.br>
2022-12-01 10:21:42 -0300

    Making changes in 'Pratica 5'

--- Prática 5/pratica5.py
+++ Prática 5/pratica5.py
@@ -5,6 +5,10 @@ import threading, time, RPi.GPIO as GPIO # Importando algumas bibliotecas
 BUTTON_GPIO = 16
 LED_GPIO = 20
 
+# Configuração dos pinos
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_GPIO, GPIO.OUT)
+
 # Definição da variável que determina o estado do LED
 blink = True
 
@@ -13,6 +17,7 @@ def red(T):
     Função que acende o LED 
     vermelho por T segundos
     '''
+    global blink
     while blink:
         print("LED piscando T =", T)
         GPIO.output(LED_GPIO, GPIO.HIGH)
@@ -25,20 +30,20 @@ def button_callback():
     Função que é chamada quando
     o botão é pressionado   
     '''
-    if GPIO.input(BUTTON_GPIO):
-        T = input('Botão pressionado!\nEscolha uma frequência para o LED piscar: ')
+    print('Botão pressionado!')
+
+    global blink
+    if blink:
+        T = int(input('Escolha uma frequência para o LED piscar: '))
         print(f'T = {T}')
         red(T)
 
 def timer_callback():
-    print('Tempo da thread esgotado')
     global blink
     blink = False
+    print('Tempo da thread esgotado')
 
 if __name__ == '__main__':
-    # Configuração dos pinos
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED_GPIO, GPIO.OUT)
     GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback = button_callback, bouncetime = 100)# Interpretação da interrupção do botão
 
